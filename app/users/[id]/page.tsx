bcd84a33abc339c8b440fe558d192cc4ed6f4284
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-11-28 23:43:33 +0700

    styles: responsive table post &data user

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -25,48 +25,52 @@ export default function UserDetailsPage({
           Details User data
         </h1>
 
-        <Link
-          href="/"
-          className="w-max rounded-xl bg-stone-200 p-[.5rem_1.3rem] text-stone-900 transition-all hover:bg-stone-400"
-        >
-          Back to home
-        </Link>
+        <div className="grid gap-4">
+          <Link
+            href="/"
+            className="w-max rounded-xl bg-stone-200 p-[.5rem_1.3rem] text-stone-900 transition-all hover:bg-stone-400"
+          >
+            Back to home
+          </Link>
 
-        <div className="flex items-start gap-8 rounded-xl border border-stone-400 bg-stone-300 p-8">
-          {isLoading ? (
-            <div className="w-full text-center italic">Loading...</div>
-          ) : (
-            <>
-              <Image
-                src="/user-circle-duotone.svg"
-                alt="user icon"
-                width={100}
-                height={100}
-                className="size-24 text-stone-900"
-              />
+          <div className="flex flex-wrap items-start gap-8 overflow-hidden rounded-xl border border-stone-400 bg-stone-300 p-8">
+            {isLoading ? (
+              <div className="w-full text-center italic">Loading...</div>
+            ) : (
+              <>
+                <Image
+                  src="/user-circle-duotone.svg"
+                  alt="user icon"
+                  width={100}
+                  height={100}
+                  className="size-24 text-stone-900"
+                />
 
-              <div className="grid grid-cols-2 gap-x-8 gap-y-4">
-                {[
-                  ["Name", user?.name],
-                  ["Email", user?.email],
-                  ["City", user?.address.city],
-                  ["Company Name", user?.company.name],
-                ].map(([key, value], index) => (
-                  <div key={index} className="grid">
-                    <span className="text-sm font-medium">{key}:</span>
-                    <h3 className="text-lg font-semibold">{value}</h3>
-                  </div>
-                ))}
-              </div>
-            </>
-          )}
+                <div className="grid items-start gap-x-8 gap-y-4 lg:grid-cols-2">
+                  {[
+                    ["Name", user?.name],
+                    ["Email", user?.email],
+                    ["City", user?.address.city],
+                    ["Company Name", user?.company.name],
+                  ].map(([key, value], index) => (
+                    <div key={index} className="grid">
+                      <span className="text-sm font-medium">{key}:</span>
+                      <h3 className="truncate text-lg font-semibold">
+                        {value}
+                      </h3>
+                    </div>
+                  ))}
+                </div>
+              </>
+            )}
+          </div>
         </div>
 
         <div className="grid gap-4">
           <h2 className="text-xl font-semibold">User post</h2>
 
-          <div className="overflow-hidden rounded-xl border border-stone-400">
-            <table className="w-full overflow-hidden">
+          <div className="overflow-hidden overflow-x-scroll rounded-xl border border-stone-400 xl:overflow-x-hidden">
+            <table className="w-full table-auto">
               <thead>
                 <tr>
                   {tableHead.map((th, index) => (
@@ -88,18 +92,18 @@ export default function UserDetailsPage({
                     </td>
                   </tr>
                 ) : (
-                  posts?.map((post) => (
+                  posts?.map((post, index) => (
                     <tr
                       key={post?.id}
                       className="border-t border-stone-400 font-normal"
                     >
                       <td className="border-r border-stone-400 p-3">
-                        {post?.id}
+                        {index + 1}
                       </td>
-                      <td className="border-r border-stone-400 p-3 capitalize">
+                      <td className="min-w-[250px] border-r border-stone-400 p-3 capitalize">
                         {post?.title}
                       </td>
-                      <td className="p-3">{post?.body}</td>
+                      <td className="min-w-[500px] p-3">{post?.body}</td>
                     </tr>
                   ))
                 )}
